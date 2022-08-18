584df90a7fe750f2dfc6c73148ffc8ce7fd44fc8
topcode111 <aldahir.martinez9197@gmail.com>
2022-08-18 13:45:00 +0900

    fixing admin

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from authorization.models import TwitterUser,TwitterAuthToken
+from .models import AutoLikeSetting,LogoUserAction,keywords,PaymentHistory
+
+
+class keywordsAdmin(admin.ModelAdmin):
+    list_display = ('keyword','user')
+    search_fields = ['keyword']
+    
+class AutoLikeSettingAdmin(admin.ModelAdmin):
+    list_display = ('action_code','user','frequency','status','end_time')
+    search_fields = ['action_code']
 
 admin.site.register(TwitterUser)
 admin.site.register(TwitterAuthToken)
+admin.site.register(PaymentHistory)
+admin.site.register(LogoUserAction)
+admin.site.register(keywords, keywordsAdmin)
+admin.site.register(AutoLikeSetting, AutoLikeSettingAdmin)
+
+
